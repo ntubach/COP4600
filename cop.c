30d0f09fc3b1ecd59bcd060708a9c939bdc2bc28
ntubach <nikotubach@gmail.com>
2016-04-19 20:51:03 -0400

    Work towards final turn-in, driver in cop.c, tester in testcop.c

--- cop.c
+++ cop.c
@@ -1,157 +1,129 @@
-/*
- *  cop4600 - does stuff
- */
+/**
+ * By: 	Niko Tubach
+ * 		Brandon Ashley
+ *		Lucas Rosa 
+ *	COP4600 - Assignment 5 
+ **/
 
-#include <linux/init.h>           // Macros used to mark up functions e.g. __init __exit
-#include <linux/module.h>         // Core header for loading LKMs into the kernel
-//#include <linux/device.h>         // Header to support the kernel Driver Model
-#include <linux/kernel.h>         // Contains types, macros, functions for the kernel
-#include <linux/fs.h>             // Header for the Linux file system support
-#include <asm/uaccess.h>	//Allows put_user
-
-//Move prototypes to .h file
-#define SUCCESS 0
-#define DEVICE_NAME "cop"	/* Dev name as it appears in /proc/devices   */
-#define BUF_LEN 80		/* Max length of the message from the device */
-int init_module(void);
-void cleanup_module(void);
-static int device_open(struct inode *, struct file *);
-static int device_release(struct inode *, struct file *);
-static ssize_t device_read(struct file *, char *, size_t, loff_t *);
-static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
+#include <linux/init.h>           /// Macros used to mark up functions e.g. __init __exit
+#include <linux/module.h>         /// Core header for loading LKMs into the kernel
+#include <linux/device.h>         /// Header to support the kernel Driver Model
+#include <linux/kernel.h>         /// Contains types, macros, functions for the kernel
+#include <linux/fs.h>             /// Header for the Linux file system support
+#include <asm/uaccess.h>          /// Required for the copy to user function
+#define  DEVICE_NAME "copchar"    /// The device will appear at /dev/ebbchar using this value
+#define  CLASS_NAME  "cop"        /// The device class -- this is a character device driver
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Niko Tubach\n\t\tBrandon Ashley\n\t\tLucas Rosa");
 MODULE_DESCRIPTION("Task to create a Linux char. device driver set to the authors by their benevolent overlord.");
 MODULE_VERSION("4.20");
 
-//Static globals
-static int Major;
-static int Device_Open = 0;
-static char msg[BUF_LEN];	/* The msg the device will give when asked */
-static char *msg_Ptr;
+static int    Major;                  		/// Stores the major device number, determined by the system
+static char   message[1024] = {0};          /// Memory for the string that is passed from userspace
+static short  size_of_message;              /// Stores size of read string
+static int    numberOpens = 0;              /// Counts the number of times the device is opened
+static struct class*  copClass  = NULL; 	/// The device-driver class struct pointer
+static struct device* copDevice = NULL; 	/// The device-driver device struct pointer
 
-struct file_operations fops = {
-	.read = device_read,
-	.write = device_write,
-	.open = device_open,
-	.release = device_release
+/// Prototype functions of character driver
+static int     dev_open(struct inode *, struct file *);
+static int     dev_release(struct inode *, struct file *);
+static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
+static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
+
+/// Lists the callback functions from fs.h you wish to use, we need open, read, write and release
+static struct file_operations fops =
+{
+   .open = dev_open,
+   .read = dev_read,
+   .write = dev_write,
+   .release = dev_release,
 };
 
-//Function called when module is loaded
-int init_module(void)
-{
-	 Major = register_chrdev(0, DEVICE_NAME, &fops);
+/// This function runs once upon initialization of driver, returns 0 upon success
+static int __init ebbchar_init(void){
+   printk(KERN_INFO "copChar: Initializing the copChar device\n");
 
-	if (Major < 0) {
-	  printk(KERN_ALERT "Registering char device failed with %d\n", Major);
-	  return Major;
-	}
+   // Register character device with 0 for dynamic major number, store the returned number to major variable
+   Major = register_chrdev(0, DEVICE_NAME, &fops);
+   // Less than 0 means failure
+   if (Major<0){
+      printk(KERN_ALERT "copChar failed to register a major number\n");
+      return Major;
+   }
+   // Else, success, print the major number
+   printk(KERN_INFO "copChar: registered successfully...major number = %d\n", Major);
 
-	printk(KERN_INFO "I was assigned major number %d. To talk to\n", Major);
-	printk(KERN_INFO "the driver, create a dev file with\n");
-	printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVICE_NAME, Major);
-	printk(KERN_INFO "Try various minor numbers. Try to cat and echo to\n");
-	printk(KERN_INFO "the device file.\n");
-	printk(KERN_INFO "Remove the device file and module when done.\n");
-	return SUCCESS;
-}
-//Function called when module is unloaded
-void cleanup_module(void)
-{
-	/* 
-	 * Unregister the device 
-	 */
-	int ret = unregister_chrdev(Major, DEVICE_NAME);
-	if (ret < 0)
-		printk(KERN_ALERT "Error in unregister_chrdev: %d\n", ret);
-}
+   // Register the device class, save result to class variable
+   copClass = class_create(THIS_MODULE, CLASS_NAME);
+   if (IS_ERR(copClass)){                			// Check for error and clean up if there is
+      unregister_chrdev(Major, DEVICE_NAME); 		//Un-register the device by major number
+      printk(KERN_ALERT "Failed to register device class\n");	//Send error message to user
+      return PTR_ERR(copClass);          			// The "proper" way to return error pointer
+   }
+   // Else, successful class registration
+   printk(KERN_INFO "copChar: device class registered correctly\n");
 
-/*
-* Methods
-*/
-
-//Called when process tries to open device file
-static int device_open(struct inode *inode, struct file *file)
-{
-	static int counter = 0;
-
-	if (Device_Open)
-		return -EBUSY;
-
-	Device_Open++;
-	sprintf(msg, "I already told you %d times Hello world!\n", counter++);
-	msg_Ptr = msg;
-	try_module_get(THIS_MODULE);
-
-	return SUCCESS;
+   // Register the device driver
+   copDevice = device_create(copClass, NULL, MKDEV(Major, 0), NULL, DEVICE_NAME);
+   if (IS_ERR(copDevice)){               // Clean up if there is an error
+      class_destroy(copClass);           // Same as above, alternatively goto statements
+      unregister_chrdev(Major, DEVICE_NAME);
+      printk(KERN_ALERT "Failed to create the device\n");
+      return PTR_ERR(copDevice);
+   }
+   // Else device driver created successfully
+   printk(KERN_INFO "copChar: device class created correctly\n"); // Made it! device was initialized
+   return 0;
 }
 
-
-
-//Called when a process closes the device file. 
-static int device_release(struct inode *inode, struct file *file)
-{
-	Device_Open--;		/* We're now ready for our next caller */
-
-	/* 
-	 * Decrement the usage count, or else once you opened the file, you'll
-	 * never get get rid of the module. 
-	 */
-	module_put(THIS_MODULE);
-
-	return 0;
+/// The cleanup function, undoes init, called upon removing the driver
+static void __exit ebbchar_exit(void){
+   device_destroy(copClass, MKDEV(Major, 0));     		// remove the device
+   class_unregister(copClass);                          // unregister the device class
+   class_destroy(copClass);                             // remove the device class
+   unregister_chrdev(Major, DEVICE_NAME);             	// unregister the major number
+   printk(KERN_INFO "copChar: Goodnight young driver\n");
 }
 
-/* 
- * Called when a process, which already opened the dev file, attempts to
- * read from it.
- */
-static ssize_t device_read(struct file *filp,	/* see include/linux/fs.h   */
-			   char *buffer,	/* buffer to fill with data */
-			   size_t length,	/* length of the buffer     */
-			   loff_t * offset)
-{
-	/*
-	 * Number of bytes actually written to the buffer 
-	 */
-	int bytes_read = 0;
-
-	/*
-	 * If we're at the end of the message, 
-	 * return 0 signifying end of file 
-	 */
-	if (*msg_Ptr == 0)
-		return 0;
-
-	/* 
-	 * Actually put the data into the buffer 
-	 */
-	while (length && *msg_Ptr) {
-
-		/* 
-		 * The buffer is in the user data segment, not the kernel 
-		 * segment so "*" assignment won't work.  We have to use 
-		 * put_user which copies data from the kernel data segment to
-		 * the user data segment. 
-		 */
-		put_user(*(msg_Ptr++), buffer++);
-
-		length--;
-		bytes_read++;
-	}
-
-	 
-	 //Most read functions return the number of bytes put into the buffer
-
-	return bytes_read;
+/// Function called whenever the device is opened
+static int dev_open(struct inode *inodep, struct file *filep){
+   numberOpens++;	//Increments open counter 
+   printk(KERN_INFO "copChar: Device has been opened %d time(s)\n", numberOpens);
+   return 0;
 }
 
-  
-//Called when a process writes to dev file: echo "hi" > /dev/hello
-static ssize_t
-device_write(struct file *filp, const char *buff, size_t len, loff_t * off)
-{
-	printk(KERN_ALERT "Sorry, this operation isn't supported.\n");
-	return -EINVAL;
+/// Function called whenever device sends data back to the terminal (user)
+static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset){
+   int error_count = 0;
+   // copy_to_user has the format ( * to, *from, size) and returns 0 on success
+   error_count = copy_to_user(buffer, message, size_of_message);
+
+   if (error_count==0){            // if true then have success
+      printk(KERN_INFO "copChar: Sent %d characters to the user\n", size_of_message);
+      return (size_of_message=0);  // clear the position to the start and return 0
+   }
+   else {
+      printk(KERN_INFO "copChar: Failed to send %d characters to the user\n", error_count);
+      return -EFAULT;              // Failed -- return a bad address message (i.e. -14)
+   }
 }
+
+/// Function called whenever the terminal (user) sends data to the device
+static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset){
+   sprintf(message, "%s(%d letters)", buffer, len);   // appending received string with its length
+   size_of_message = strlen(message);                 // store the length of the stored message
+   printk(KERN_INFO "copChar: Received %d characters from the user\n", len);
+   return len;
+}
+
+/// Function called whenever the userspace program releases the device
+static int dev_release(struct inode *inodep, struct file *filep){
+   printk(KERN_INFO "copChar: Device successfully closed\n");
+   return 0;
+}
+
+/// Defines the init and cleanup functions for use via init.h
+module_init(ebbchar_init);
+module_exit(ebbchar_exit);
--- testcop.c
+++ testcop.c
@@ -1,12 +1,8 @@
 /**
- * @file   testebbchar.c
- * @author Derek Molloy
- * @date   7 April 2015
- * @version 0.1
- * @brief  A Linux user space program that communicates with the ebbchar.c LKM. It passes a
- * string to the LKM and reads the response from the LKM. For this example to work the device
- * must be called /dev/ebbchar.
- * @see http://www.derekmolloy.ie/ for a full description and follow-up descriptions.
+ * By: 	Niko Tubach
+ * 		Brandon Ashley
+ *		Lucas Rosa 
+ *	COP4600 - Assignment 5 
 */
 #include<stdio.h>
 #include<stdlib.h>
@@ -14,14 +10,14 @@
 #include<fcntl.h>
 #include<string.h>
 
-#define BUFFER_LENGTH 256               ///< The buffer length (crude but fine)
-static char receive[BUFFER_LENGTH];     ///< The receive buffer from the LKM
+#define BUFFER_LENGTH 1024               /// The buffer length 
+static char receive[BUFFER_LENGTH];     /// The receive buffer
 
 int main(){
    int ret, fd;
    char stringToSend[BUFFER_LENGTH];
    printf("Starting device test code example...\n");
-   fd = open("/dev/ebbchar", O_RDWR);             // Open the device with read/write access
+   fd = open("/dev/copchar", O_RDWR);             // Open the device with read/write access
    if (fd < 0){
       perror("Failed to open the device...");
       return errno;
